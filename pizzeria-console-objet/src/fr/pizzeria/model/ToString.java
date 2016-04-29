efa719b46db93a5454ce6a52752661ec42b60be2
Olivier LE FLOHIC <olivier.a.leflohic@gmail.com>
2016-04-29 17:00:22 +0200

    modifications mineures

--- pizzeria-console-objet/src/fr/pizzeria/model/ToString.java
+++ pizzeria-console-objet/src/fr/pizzeria/model/ToString.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation utilisée sur les variables de la classe Pizza.
+ * Indique si la variable en question doit être incluse dans la méthode toString().
+ * @author oleflohic
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.FIELD })
 public @interface ToString {
